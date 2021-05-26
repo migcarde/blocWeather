0ce57f35f62cebae3330181adf398e810063328f
migcarde <mcaraballodelarosa@outlook.com>
2021-05-26 08:11:31 +0200

    Current weather #2
    - Added repository contract.

--- packages/domain/lib/repository/current_weather_repository_contract.dart
+++ packages/domain/lib/repository/current_weather_repository_contract.dart
@@ -1,3 +1,8 @@
-class CurrentWeatherRepositoryContract {
-  Future<Either<
+import 'package:dartz/dartz.dart';
+import 'package:domain/business/current_weather_business.dart';
+import 'package:domain/errors/current_weather_error.dart';
+
+abstract class CurrentWeatherRepositoryContract {
+  Future<Either<CurrentWeatherFailure, CurrentWeatherBusiness>> getWeather(
+      String city);
 }
